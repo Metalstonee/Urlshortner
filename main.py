bb9080b302b566af13c8ea85bd10171c7f497cdb
Sina <monfaredsina2@gmail.com>
2025-03-17 04:03:00 -0700

    Version V1 Finalized

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from validation import valid
+from QR_CODE import genqr
+import pyshorteners
 url = str(input("enter the url  "))
+valid(url)
+s = pyshorteners.Shortener()
+short = s.tinyurl.short(url)
+genqr(url , "Shortendqr.png")
+print(short)
+f = open("Link.txt", "w")
+f.write(f"Heres the Link !  =>  {short}")
+f.close()
